7157fb3a35611f83e8b1b8835abea4d099405bea
huangfuyuru <864134710@qq.com>
2020-03-11 14:36:38 +0800

    a

--- src/11-password-visual/main.js
+++ src/11-password-visual/main.js
@@ -1,20 +1,14 @@
 $(function(){
-  var $button = $('input'),
-      time = 6,
-      timer;
+  var $pass = $('.pass'),
+      $eye = $('.eye');
 
-  timer = setInterval(function(){
-    time--;
-    if(time == 0){
-      clearInterval(timer);
-      $button.val(`确定`);
-      $button.removeAttr('disabled');
-    }else{
-      $button.val(`确定(${time}s)`);
-    }
-  },1000);
+  $eye.mouseover(function(){
+    $pass.attr('type','text');
+    $eye.addClass('eye2')
+  })
 
-  $button.click(function(){
-    alert("已点击确认");
+  $eye.mouseout(function(){
+    $pass.attr('type','password');
+    $eye.removeClass('eye2');
   })
 })
