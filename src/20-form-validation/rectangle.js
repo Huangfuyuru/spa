20b14fe245ba7987d17fab484196474945bacae8
huangfuyuru <864134710@qq.com>
2020-03-17 15:32:58 +0800

    a

--- src/20-form-validation/rectangle.js
+++ src/20-form-validation/rectangle.js
@@ -16,6 +16,52 @@ $(function(){
     $perimeter.val(rect.perimeter());
     $area.val(rect.area());
   });
+  
+  //字符检验
+  //1.event keypress
+  //2.event argument get key value,e.key e.value
+  //3.ilegal key filter 非法的键屏蔽掉 e.preventDefault
+  //4.合法字符 非法出现 (它出现的位置非法)
+  $width.keypress(function(e){
+    if(/[abcdf-zABCDF-Z`~!@#$%^&*()\-=_+[\]{}|;:'",<>/?\\]/.test(e.key)){
+      e.preventDefault();
+      return;
+    }
+
+  });
+  $height.keypress(function(e){
+    if(/[abcdf-zABCDF-Z`~!@#$%^&*()\=_+[\]{}|;:'",<>/?\\]/.test(e.key)){
+      e.preventDefault();
+      return;
+   }
+    //合法字符 e
+    //允许出现在非科学计数法的数字末尾
+    //允许出现在非科学计数法的数字中间
+    
+    //不允许出现在非科学计数法的数字前面
+    //不允许出现在空文本中
+    //不允许出现在负号后面
+    //不允许出现在科学计数法的数字的末尾
+    //不允许出现在科学计数法的数字的前面
+    //不允许出现在科学计数法的数字的中间
+    
+    var pos = e.target.selectionStart,
+        con = e.target.value;
+    if(e.key === 'e'){
+      if(pos === 0 || con.indexOf('e')!==-1 || con.indexOf('E')!==-1){
+        e.preventDefault();
+        return;
+      }
+      if(pos === 1 && con.substring(0,1) === '-'){
+        e.preventDefault();
+        return;
+      }
+    }
+    
+    //合法字符E
+    //合法字符.
+  });
+
 
   $width.focusout(function(){
     //if(!validate(width)) select this;
